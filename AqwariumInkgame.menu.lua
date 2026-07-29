2f7e2b790f7b5cf419f559658301e78d5d0236a3
QwertyX01 <loksiq1201@gmail.com>
2026-07-29 09:11:52 +0500

    Update AqwariumInkgame.menu.lua

--- AqwariumInkgame.menu.lua
+++ AqwariumInkgame.menu.lua
@@ -177,7 +177,7 @@ rightCorners.Parent = rightPanel
 -- ============================================================
 --  ВКЛАДКИ (Game, Players, Misc, Combat)
 -- ============================================================
-local tabNames = {"Game", "Player","Combat", "Misc"}   -- изменено
+local tabNames = {"Game", "Плеер","Combat", "Misc"}   -- изменено
 local tabButtons = {}
 local rightContentFrames = {}
 
